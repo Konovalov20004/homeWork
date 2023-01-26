1baa34b3276a631dece57b609fb7dd6cc5915d47
Sergey <konovalov20004@gmail.com>
2023-01-26 20:08:31 +0300

    intermediateResult

---
+++ Seminar4/task2/task2.cs
@@ -0,0 +1,19 @@
+﻿int enter (string message){
+    Console.Write($"{message} > ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int sumNumber (int n) {
+    int sum = 0;
+    int x = n;
+    for (int i = 0; x != i;) {
+        int c = x % 10;
+        sum += c;
+        x /= 10;
+    }
+    return(sum);
+}
+
+int number = enter("Введите число, чтобы узнать сумму цифр");
+int sum = sumNumber(number);
+Console.WriteLine(sum);
---
+++ Seminar4/task3/task3.cs
@@ -0,0 +1,29 @@
+﻿/*int enter (string message){
+    Console.WriteLine($"Введити числа без пробелов > ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int length (int a){
+    a = ;
+    for (int c = 0; b != 0;c++){
+        a /=10 ;
+    }
+    return(a);
+}
+*/
+
+int length = Convert.ToInt32(Console.ReadLine());
+int enter = Convert.ToInt32(Console.ReadLine());
+int[] x = new int[length];
+for (int i = 0; i < length; i++){
+    x[i] = fillingAr(enter);
+}
+int fillingAr (int a){
+    int v = enter % 10;;
+    enter /= 10;
+    return(v);  
+}
+
+foreach (int s in x){
+    Console.Write(s);
+}
