65a0eceac357b3767156d01c27a6904b3f14dbe0
Sergey <konovalov20004@gmail.com>
2023-01-27 18:08:49 +0300

    seminar4done

---
+++ Seminar4/disabledTask3/disabledTask3.cs
@@ -0,0 +1,47 @@
+﻿int entere (string message){
+    Console.WriteLine($"{message} > ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int enter = entere("Введите числа без пробелов");
+int longa = length(enter);
+int[] x = new int[longa];
+
+for ( int b = 0; longa > b; b++){
+    int q = fillingAr(longa);
+    x[b] = q;
+}
+Array.Reverse(x);
+foreach (int s in x){
+    Console.Write(s);
+}
+
+
+int length (int a){
+    a = enter;
+    int count = 0;
+    for (int c = 0; a != 0;c++){
+        a /=10 ;
+        count++;
+    }
+    return(count);
+}
+
+
+//int length = Convert.ToInt32(Console.ReadLine());
+
+int number = enter;
+int fillingAr (int a){
+    a = longa;
+    //for (int o = 0; o < a; o++){
+        int v = enter % 10;;
+        enter /= 10;
+        a--;
+        return(v);  
+    //}
+}
+
+for ( int b = 0; longa > b; b++){
+    int q = fillingAr(longa);
+    x[b] = q;
+}
--- Seminar4/task3/task3.cs
+++ Seminar4/task3/task3.cs
@@ -1,29 +1,14 @@
-﻿/*int enter (string message){
-    Console.WriteLine($"Введити числа без пробелов > ");
-    return Convert.ToInt32(Console.ReadLine());
+﻿int[] x = new int[8];
+for (int i = 0; i < 8; i++) {
+    x[i] = y();
 }
 
-int length (int a){
-    a = ;
-    for (int c = 0; b != 0;c++){
-        a /=10 ;
-    }
-    return(a);
-}
-*/
-
-int length = Convert.ToInt32(Console.ReadLine());
-int enter = Convert.ToInt32(Console.ReadLine());
-int[] x = new int[length];
-for (int i = 0; i < length; i++){
-    x[i] = fillingAr(enter);
-}
-int fillingAr (int a){
-    int v = enter % 10;;
-    enter /= 10;
-    return(v);  
+int y (){
+    Console.WriteLine("Введите число");
+    int v = Convert.ToInt32(Console.ReadLine());
+    return v;
 }
 
-foreach (int s in x){
-    Console.Write(s);
+foreach (int a in x){
+    Console.Write($"{a} ");
 }
