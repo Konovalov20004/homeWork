48c8decf9ba36072b9e7d62391e90165ce582214
Sergey <konovalov20004@gmail.com>
2023-02-02 14:47:44 +0300

    Seminar6done

---
+++ Seminar6/Task1/task1.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите числа через пробел > ");
+string[] a = Console.ReadLine().Split(" ");
+int[] b = new int[a.Length];
+int count = 0;
+for (int c = 0; c < a.Length; c++) {
+    b[c] = Convert.ToInt32(a[c]);}
+for (int c = 0; c < b.Length; c++){
+        if (b[c] > 0) count++;}
+Console.WriteLine($"Количество натуральных чисел: {count}");
---
+++ Seminar6/Task2/task2.cs
@@ -0,0 +1,22 @@
+﻿decimal[] coordinates = new decimal[4];
+string[] namePunkt = {"b1", "k1", "b2", "k2"};
+string[] numberLine = {"первой", "второй"};
+int count = 0;
+for (int c = 0; c < 2; c++){
+    Console.WriteLine($"Координаты {numberLine[c]} прямой: ");
+    for (int v = 0; v < 2; v++) {
+        Console.Write($"Введите точку {namePunkt[count]} >");
+        coordinates[count] = Convert.ToDecimal(Console.ReadLine());
+        count++;
+    }
+
+decimal x = 0;
+decimal y = 0;
+if (coordinates[1] - coordinates[3] == 0 && -coordinates[0] + coordinates[2] == 0) {
+    Console.WriteLine($"\n Ваши прямые совпадают");
+} else if (coordinates[1] - coordinates[3] == 0 || -coordinates[0] + coordinates[2] == 0) {
+        Console.WriteLine($"\n Ваши прямые не пересекаются");
+    } else {
+        x =(-coordinates[0] + coordinates[2])/(coordinates[1] - coordinates[3]);
+        y = coordinates[3] * x + coordinates[2];}
+Console.WriteLine($"Координаты пересечения заданных прямых: {x};{y}");
